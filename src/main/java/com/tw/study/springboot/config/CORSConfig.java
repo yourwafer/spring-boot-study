c20ed6276241a9d3f52c6abedf35fad3573385f4
HongweiWei <hwwei@wei-hongwei.local>
2016-11-08 17:41:17 +0800

    添加CORS支持

---
+++ src/main/java/com/tw/study/springboot/config/CORSConfig.java
@@ -0,0 +1,24 @@
+package com.tw.study.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by hwwei on 2016/11/8.
+ */
+@Configuration
+public class CORSConfig {
+
+    @Bean
+    public static WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
+    }
+}
